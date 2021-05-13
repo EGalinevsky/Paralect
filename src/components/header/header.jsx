47ca57c51490ce9a0f5ac76b73d180b83d37bea8
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-05-13 21:22:04 +0300

    useStateHeader

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Frame from "./../../other/img/Frame.svg";
 import Search from "./../../other/img/image.svg";
 import s from "./header.module.css";
 
 export const Header = () => {
+
+  const[name, setName] = useState('')
+
+  const HandlerChange = (e) =>{
+    setName(e.target.value)
+  }
+ 
+  
+
   return (
     <div className={s.header}>
       <img className={s.header__img} src={Frame} alt="" />
       <div className={s.header__search}>
       <img className={s.search__img} src={Search} alt="" />
         <input
+          value={name}
           className={s.search__input}
+          onChange={HandlerChange}
           type="text"
           placeholder="Enter GitHub username"
-        />
-        
+        />        
       </div>
     </div>
   );
